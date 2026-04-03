4d254145bfa00f33db33ca7f92fd78274f5e8df6
Rosalie <remty516@gmail.com>
2026-04-04 00:51:45 +0200

    Fix color preview for workshop

--- src/settings_utils.ts
+++ src/settings_utils.ts
@@ -40,47 +40,60 @@ export const HUE = {
   MAX: 360,
 } as const;
 
+/** 2px white square */
+const whiteImagePath = `data/ui_gfx/empty_white.png`;
+
 export const makeSettingHue = (setting: ModSettingHue): ModSettingHue => ({
   ...setting,
-// wrapper for mod_setting_number
+  // wrapper for mod_setting_number
   draw: function ({ gui, in_main_menu, im_id }) {
-  const sliderWidth = 64;
-  const [nameWidth] = GuiGetTextDimensions(gui, this.ui_name);
+    const sliderWidth = 64;
+    const [nameWidth] = GuiGetTextDimensions(gui, this.ui_name);
 
-  // gradient image behind slider
-  const sliderOffset = settings.mod_setting_group_x_offset + nameWidth + 1;
-  const sliderImagePath = `mods/${MOD_ID}/assets/hue_slider.png`;
-  GuiZSetForNextWidget(gui, 1);
-  GuiOptionsAddForNextWidget(gui, GUI_OPTION.Layout_NextSameLine);
-  GuiImage(gui, im_id, sliderOffset, -1, sliderImagePath, 1, 1, 10);
+    const drawSlice = ([r, g, b]: number[], x: number, width: number) => {
+      GuiZSetForNextWidget(gui, 1);
+      GuiColorSetForNextWidget(gui, r, g, b, 1);
+      GuiOptionsAddForNextWidget(gui, GUI_OPTION.Layout_NextSameLine);
+      GuiImage(gui, im_id, x, -1, whiteImagePath, 1, width / 2, 5);
+    };
 
-  // get preview color
-  let [h, s, v] = [
-    ModSettingGetNextValue(`${MOD_ID}.${this.id}`),
-    this.preview_s ?? 1,
-    this.preview_v ?? 1,
-  ];
-  if (typeof h != "number") h = this.value_default ?? 0;
-  const [r, g, b] = convertHSVtoRGB(h, s, v);
+    // gradient behind slider
+    const sliderOffset = settings.mod_setting_group_x_offset + nameWidth + 1;
+    drawSlice([1, 0, 0], sliderOffset, 4);
+    const hueGradientWidth = sliderWidth - 7;
+    for (let i = 0; i < hueGradientWidth; i++) {
+      const hue = ((HUE.MAX - HUE.MIN) * i) / (hueGradientWidth - 1) + HUE.MIN;
+      const rgb = convertHSVtoRGB(hue, 1, 1);
+      drawSlice(rgb, sliderOffset + 4 + i, 1);
+    }
+    drawSlice([1, 0, 0], sliderOffset + sliderWidth - 3, 5);
 
-  // color preview next to slider
-  const whiteImagePath = `mods/${MOD_ID}/assets/1px_white.png`;
-  GuiColorSetForNextWidget(gui, r, g, b, 1);
-  GuiOptionsAddForNextWidget(gui, GUI_OPTION.Layout_NextSameLine);
-  GuiImage(gui, im_id, sliderOffset + sliderWidth + 3, -1, whiteImagePath, 1, 10, 10);
+    // get preview color
+    let [h, s, v] = [
+      ModSettingGetNextValue(`${MOD_ID}.${this.id}`),
+      this.preview_s ?? 1,
+      this.preview_v ?? 1,
+    ];
+    if (typeof h != "number") h = this.value_default ?? 0;
+    const [r, g, b] = convertHSVtoRGB(h, s, v);
 
-  // hacky way to add padding between slider and its value
-  let spacePaddingWidth = 0;
-  let spacePadding = "";
-  while (spacePaddingWidth < 10) {
-    spacePadding += " ";
-    const [w] = GuiGetTextDimensions(gui, spacePadding);
-    spacePaddingWidth = w;
-  }
+    // color preview next to slider
+    GuiColorSetForNextWidget(gui, r, g, b, 1);
+    GuiOptionsAddForNextWidget(gui, GUI_OPTION.Layout_NextSameLine);
+    GuiImage(gui, im_id, sliderOffset + sliderWidth + 3, -1, whiteImagePath, 1, 5, 5);
 
-  settings.mod_setting_number(MOD_ID, gui, in_main_menu, im_id, {
-    ...this,
-    value_display_formatting: spacePadding + (this.value_display_formatting ?? ""),
-  });
+    // hacky way to add padding between slider and its value
+    let spacePaddingWidth = 0;
+    let spacePadding = "";
+    while (spacePaddingWidth < 10) {
+      spacePadding += " ";
+      const [w] = GuiGetTextDimensions(gui, spacePadding);
+      spacePaddingWidth = w;
+    }
+
+    settings.mod_setting_number(MOD_ID, gui, in_main_menu, im_id, {
+      ...this,
+      value_display_formatting: spacePadding + (this.value_display_formatting ?? ""),
+    });
   },
 });
